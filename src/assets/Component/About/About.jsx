924f265196883143d5083565600469bc771bea50
Mmohima Modak <mohimamodak316@gmail.com>
2025-05-20 23:32:08 +0600

    Added mt-5 class for top margin

--- src/assets/Component/About/About.jsx
+++ src/assets/Component/About/About.jsx
@@ -17,15 +17,9 @@ export default function About() {
     }, []);
 
     return (
-        <div  id='/About' className='bg-gradient-to-br from-red-50 via-pink-200 to-red-50 py-14 px-6 lg:px-20 relative'>
+        <div id='/About' className='bg-gradient-to-br from-red-50 via-pink-200 to-red-50 py-14 px-6 lg:px-20 relative'>
             <div data-aos="fade-up" className='lg:flex items-center justify-center gap-12'>
-                <div className='flex items-center justify-center'>
-                    <img
-                        src={image}
-                        alt="Nail Polish"
-                        className=' object-contain shadow-2xl rounded-md hover:scale-90 duration-1000'
-                    />
-                </div>
+               
                 <div className=' text-center md:text-left'>
                     <h2 className='text-pink-500 text-4xl lg:text-5xl text-center font-bold'>
                         Make Every Move Sparkle
@@ -33,10 +27,18 @@ export default function About() {
                     <p className='text-xl md:text-2xl text-gray-600 my-6 max-w-md'>
                         Discover Our Luxe Palette — Crafted for Bold, Beautiful Statements.                    </p>
                     <Link to='/Contact' smooth={true} duration={1000}>
-                                                        <button className="bg-pink-500 text-white px-6 py-3 font-medium rounded-md hover:bg-pink-600 transition  shadow-md hover:scale-105 duration-1000">
-                                                            Order Now
-                                                        </button>
-                                                    </Link>
+                        <button className="bg-pink-500 text-white px-6 py-3 font-medium rounded-md hover:bg-pink-600 transition  shadow-md hover:scale-105 duration-1000">
+                            Order Now
+                        </button>
+                    </Link>
+                </div>
+
+                 <div className='flex items-center justify-center'>
+                    <img
+                        src={image}
+                        alt="Nail Polish"
+                        className=' object-contain mt-5 lg:mt-0 shadow-2xl rounded-md hover:scale-90 duration-1000'
+                    />
                 </div>
 
             </div>
